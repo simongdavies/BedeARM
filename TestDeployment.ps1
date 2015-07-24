e28a8fe255af28c6172814f0a035cedffa4cd452
Simon Davies <simongdavies@hotmail.com>
2015-07-24 14:00:48 +0100

    Initial Commit

---
+++ TestDeployment.ps1
@@ -0,0 +1,22 @@
+﻿param (
+[string]$ResourceGroupName,
+[string]$Location='northeurope'
+)
+Switch-AzureMode AzureResourceManager
+if (!(Get-AzureAccount))
+{ 
+        Add-AzureAccount
+}
+try
+{
+    New-AzureResourceGroup -Name $ResourceGroupName -Location $Location
+
+    # Deploy a new VNet, DB Server and DB 
+
+    New-AzureResourceGroupDeployment -Name 'allnew' -ResourceGroupName $ResourceGroupName -TemplateFile .\MasterTemplate.json -TemplateParameterFile .\azuredeploy-parameters-minimal-new-vnet-sqlserver-sqldatabase.json
+
+}
+catch
+{
+    Write-Output $_
+}
